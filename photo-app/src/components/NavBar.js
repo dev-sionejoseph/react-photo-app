4dd649f25ae4e2478793b56e30e9c5f2f4fc765d
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-18 18:44:00 -0500

    reworked nav component basics

--- photo-app/src/components/NavBar.js
+++ photo-app/src/components/NavBar.js
@@ -1,26 +1,32 @@
 import React from 'react'
 
 export default function NavBar() {
-    onHome = (e) => {
-        console.log("home clicked");
-        // add steps to open home component
-    }
-    onProfile = (e) => {
-        console.log("profile clicked");
-        // add steps to open profile component
-    }
-    onSettings = (e) => {
-        console.log("settings clicked");
-        // add steps to open settings component
-    }
+
+        
+        const onHome = (e) => {
+            console.log("home clicked");
+            // add steps to open home component
+        }
+        const onProfile = (e) => {
+            console.log("profile clicked");
+            // add steps to open profile component
+        }
+        const onSettings = (e) => {
+            console.log("settings clicked");
+            // add steps to open settings component
+        }
+    
+        // this.onHome = this.onHome.bind(this);
+        // this.onProfile = this.onProfile.bind(this);
+        // this.onSettings = this.onSettings.bind(this);
 
     return (
-        <div>
+        <div id="navWrap">
             <img alt="logo" src="#" />
             <nav>
                 <li className="navItem" id="home"><button id="homeButton" onClick={onHome}></button></li>
-                <li className="navItem" id="profile"><button id="homeButton" onClick={onProfile}></button></li>
-                <li className="navItem" id="settings"><button id="homeButton" onClick={onSettings}></button></li>
+                <li className="navItem" id="profile"><button id="profileButton" onClick={onProfile}></button></li>
+                <li className="navItem" id="settings"><button id="settingsButton" onClick={onSettings}></button></li>
             </nav>
         </div>
     )
